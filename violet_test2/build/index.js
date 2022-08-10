62decb62531395b0de56075e62f14549a81f4b48
Redmountain2018 <2826903003@qq.com>
2022-08-10 18:27:31 +0800

    Site updated: 2022-08-10 18:27:18

---
+++ violet_test2/build/index.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", () => {
+    let cover = document.querySelector("#cover");
+    let translateY = window.scrollY / 3;
+    cover.style.transform = "translateY(" + translateY.toString() + "px)";
+    let opacity = 1 - window.scrollY / 1000;
+    if (opacity < 0)
+        opacity = 0;
+    cover.style.opacity = opacity.toString();
+});
+window.addEventListener("DOMContentLoaded", () => {
+    let btns = document.querySelectorAll(".button");
+    for (let i = 0; i < btns.length; i++) {
+        const btn = btns.item(i);
+        btn.addEventListener("mousemove", (event) => {
+            btn.style.setProperty("--shadow-x", event.offsetX + "px");
+            btn.style.setProperty("--shadow-y", event.offsetY + "px");
+        });
+    }
+});
